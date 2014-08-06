fe251d31c880cdacecc0fd702588d9a386286e6a
Jacky Lee <jackylee@VM94.(none)>
2014-08-06 18:58:10 +0800

    I update my modified code for lab-2.

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,7 +9,11 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
-
+          ldr r1,[r0]
+          cmp r1,#1
+          beq lock_mutex
+          mov r1,#1
+          str r1,[r0]
         @ END CODE INSERT
 	bx lr
 
@@ -19,7 +23,8 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+         mov r1,#0
+         str r1,[r0]
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
